241009f5b14d6bece6c22eccf63ba60ac3921232
Reda-Ganoutre01 <bganoutre52@gmail.com>
2025-02-26 17:24:10 +0000

    added selectors

---
+++ frontend/src/context/AuthProvider.jsx
@@ -0,0 +1,20 @@
+import {AuthContext} from "./AuthContext.jsx";
+import {useDispatch, useSelector} from "react-redux";
+import {useContext} from "react";
+import authenticateUser from "../features/auth/actions/authenticateUser.js";
+import {logout} from "../features/auth/AuthReducer.js";
+
+
+export default function AuthProvider({children}) {
+
+    const dispatch = useDispatch();
+    const {user , isAuthenticated} = useSelector((state) => state.auth);
+
+    const login = (credentials) => {
+        dispatch(authenticateUser(credentials))
+    }
+
+    return <AuthContext.Provider value={{user : user , login: login , isAuthenticated: isAuthenticated}}>
+        {children}
+    </AuthContext.Provider>
+}
