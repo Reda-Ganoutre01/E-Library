31f0a54402d06bfa1f1dd21d96f9718efffc6604
Reda-Ganoutre01 <bganoutre52@gmail.com>
2025-02-27 23:08:30 +0000

    added features login and validate

--- frontend/src/features/auth/actions/authenticateUser.js
+++ frontend/src/features/auth/actions/authenticateUser.js
@@ -1,18 +1,13 @@
-import {createAsyncThunk} from "@reduxjs/toolkit";
-import AuthService        from "../../../services/AuthService";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import AuthService from "../../../services/AuthService";
 
-
-const authenticateUser = createAsyncThunk('auth/login' , async (credentials, { rejectWithValue }) => {
-    try
-    {
-        const response = await AuthService.authenticate(credentials);
-        return response.data;
-    }
-    catch (error)
-    {
-        rejectWithValue(error.response?.data || error.message);
-    }
+const authenticateUser = createAsyncThunk("auth/login", async (credentials, { rejectWithValue }) => {
+  try {
+    const response = await AuthService.authenticate(credentials);
+    return response.data;
+  } catch (error) {
+    return rejectWithValue(error.response?.data || "Authentication failed");
+  }
 });
-
 
 export default authenticateUser;
--- frontend/src/services/AuthService.js
+++ frontend/src/services/AuthService.js
@@ -4,12 +4,13 @@ class AuthService
 {
     constructor()
     {
-        this.http = axios.create({baseURL : "/api/v1/auth"});
+        this.http = axios.create({baseURL : "http://localhost:8080/api/v1/auth"});
     }
     async authenticate(credintials)
     {
         return await this.http.post("/authenticate", credintials);
     }
+    
 }
 
 export default new AuthService()
